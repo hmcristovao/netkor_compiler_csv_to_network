01532e653a1220b64672aa623b98e297187a7ec0
klauskly <klauskly_Cw@hotmail.com>
2019-11-05 16:10:01 -0300

    Mudando para um formato mais OO.

---
+++ netkor_compiler_csv_network/src/setting/Configuration.java
@@ -0,0 +1,7 @@
+package setting;
+
+public interface Configuration {	
+	String csvFileInput = "exemplo.csv";			//Para testar o teste do registro netKor, basta modificar			
+	String mapFile = "exemplo.map";					//Basta modificar o .csv e o .map como exemplo2
+	String csvFileOutput = "exemplo.net";
+}
